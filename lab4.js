0b5ba5cf32624fcdf74791b85780ace2f6f4cbe1
cameron <johnson.cameron.m@gmail.com>
2018-05-06 22:40:52 -0400

    commit for part 2

--- lab4.js
+++ lab4.js
@@ -1,5 +1,82 @@
 "use strict";
 
+class AddressBook {
+    constructor(contactList){
+        this.contactList = [
+            // {name: "John", email: "johnny@gmail.com", phone: "313-590-5545", relation: "Brother"},
+            // {name: "Timmy", email: "biggiesmalls@yahoo.com", phone: "563-343-0084", relation: "Boss" },
+            // {name: "Ms. T", email: "lilT@TilaTequilla.com", phone: "545-662-6234", relation: "TV Lady"}
+        ];
+    }
+
+    add() {
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let phone = document.getElementById("phone").value;
+        // let relation = document.getElementById("option").value;
+
+        if(name !== "" && email !== "" && phone !== ""){
+            const info = {
+              name: name,
+              email: email,
+              phone: phone
+            //   relation: relation
+            }
+      
+            //create a new instance of new contact and push into contacts array
+            const newContact = new Contact(info.name, info.email, info.phone);
+            addressBook.contactList.push(newContact);
+        
+            // reset input values
+            addressBook.clearInputs();
+            
+            //display all the contacts on the page
+            addressBook.display();
+      
+        } else { //let them know they didn't enter enough info
+            alert("Please enter a new contact name as well as an email and phone.")
+          }
+          console.log(addressBook.contactList);
+    }
+
+    clearInputs(){
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        // document.getElementById("relation").value = "";
+      }
+
+    display(){
+        let arrayVariable = addressBook.contactList;
+        let arrayLength = arrayVariable.length;
+        let newContactCard;
+
+        let contactsContainer =document.getElementById('list');
+        contactsContainer.innerHTML ="";
+
+        for(let i = 0; i < arrayLength; i++){
+            newContactCard = document.createElement('section');
+            newContactCard.className = 'contactList';
+            newContactCard.innerHTML = `
+            <p>Name: ${arrayVariable[i].name}</p>
+            <p>Email: ${arrayVariable[i].email}</p>
+            <p>Phone: ${arrayVariable[i].phone}</p>
+            <i class="fa fa-trash-alt trashCan"></i>`;
+
+            document.getElementById('list').appendChild(newContactCard);
+        }
+    }
+
+    deleteAt(index){
+        console.log(`removed the contact ${contactList[index].name} in the number ${index} index.`)
+        contactList.splice(index, 1);
+    }
+
+    deleteByName(i) {
+        this.contacts.splice(i, 1);
+    }
+}
+    
 class Contact {
     constructor(name, email, phone, relation){
         this.name = name;
@@ -9,60 +86,8 @@ class Contact {
     }
 }
 
-class AddressBook {
-    constructor(){
-        this.contactList = [];
-    }
-    add(info){
-        let newContact = new Contact (info.name, info.email, info.phone, info.relation);
-        contactList.push(newContact);
-        console.log(this.contactList);
-    }
-
-    deleteAt(index){
-        console.log(`removed the contact ${contactList[index].name} in the number ${index} index.`)
-        contactList.splice(index, 1);
-    }
-
-    print(){
-    contactList.forEach((contact) => {
-        console.log(contact);
-     });
-    }
-}
-
-const contactList = [
-    {name: "John", email: "johnny@gmail.com", phone: "313-590-5545", relation: "Brother"},
-    {name: "Timmy", email: "biggiesmalls@yahoo.com", phone: "563-343-0084", relation: "Boss" },
-    {name: "Ms. T", email: "lilT@TilaTequilla.com", phone: "545-662-6234", relation: "TV Lady"}
-];
-let ans = true;
 const addressBook = new AddressBook();
-while (ans !== "quit"){
-    ans = prompt("Add, Delete, Print, or Quit?");
-    switch(ans.toLowerCase()){
-        case "add":
-            let info = {
-                name: prompt("Please enter contacts name"),
-                email: prompt("Please enter contact email address"),
-                phone: prompt("Please enter the contacts phone number"),
-                relation: prompt("Enter your relation to the contact"),
-            }
-            addressBook.add(info);
-            break;
-        case "delete":
-            let index = prompt("What index number would you like to delete?");
-            addressBook.deleteAt(index);
-            break;
-        case "print":
-            addressBook.print();
-            break;
-        case "quit":
-            break;
-    }
 
-}
+document.getElementById("add").addEventListener("click", addressBook.add); 
 
-
-
-
+addressBook.display();
